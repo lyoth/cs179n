09a785f77b23aa8765633072d536de6bcf41c087
vpang002 <vpang002@ucr.edu>
2015-01-22 19:10:50 -0800

    Added Left and Right Cameras

---
+++ PirateWars/Assets/Scripts/ChangeCameras.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeCameras: MonoBehaviour
+{
+	public Camera cameraMain;
+	public Camera cameraLeft;
+	public Camera cameraRight;
+
+	void Start() {
+
+	}
+	void Update () {
+		if(Input.GetKey("q")){
+			cameraMain.camera.active = false;
+			cameraLeft.camera.active = true;
+			cameraRight.camera.active = false;
+		}
+		if(Input.GetKey("e")){
+			cameraMain.camera.active = false;
+			cameraLeft.camera.active = false;
+			cameraRight.camera.active = true;
+		}
+		if(Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d")){
+			cameraMain.camera.active = true;
+			cameraLeft.camera.active = false;
+			cameraRight.camera.active = false;
+		}
+	}
+}
