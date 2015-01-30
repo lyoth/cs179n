32b7bfee851ffa6a8a93c0b49f512a8d7ca9dc1d
vpang002 <vpang002@ucr.edu>
2015-01-29 18:02:36 -0800

    Revert 471fe5a..8ae7bf0
    This rolls back to commit 471fe5aa9c9810ed1456b378d5e64591061665a3.

--- PirateWars/Assets/Scripts/Enemies/EnemyAI.cs
+++ PirateWars/Assets/Scripts/Enemies/EnemyAI.cs
@@ -7,7 +7,7 @@ public class EnemyAI : MonoBehaviour {
 	private GameObject player;							// Reference to the player.
 	//private NavMeshAgent nav;								// Reference to the nav mesh agent.
 	public float patrolSpeed = 3f;							// The nav mesh agent's speed when patrolling.
-	public float rotationSpeed = 2f;						
+	//public float rotationSpeed = 2f;						
 	public float chaseSpeed = 5f;							// The nav mesh agent's speed when chasing.
 	//public float chaseWaitTime = 5f;						// The amount of time to wait when the last sighting is reached.
 	//public float patrolWaitTime = 1f;						// The amount of time to wait when the patrol way point is reached.
@@ -69,9 +69,6 @@ public class EnemyAI : MonoBehaviour {
 
 	void Chasing() {
 		state = CHASE;
-		Quaternion rotation = Quaternion.LookRotation(player.transform.position - transform.position);
-		//rotate us over time according to speed until we are in the required rotation
-		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
 		Move (transform, player.transform, chaseSpeed);
 	}
 	
@@ -87,18 +84,15 @@ public class EnemyAI : MonoBehaviour {
 		}
 		//TODO: get rotation working correctly
 		//create the rotation we need to be in to look at the target
-		Quaternion rotation = Quaternion.LookRotation(wayPoint.position - transform.position);
+		//Quaternion rotation = Quaternion.LookRotation(wayPoint.position - transform.position);
 		//rotate us over time according to speed until we are in the required rotation
-		transform.rotation = Quaternion.AngleAxis(270, Vector3.down);
-		//transform.Rotate (new Vector3 (rotation.x, rotation.y, rotation.z));
-		//transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
 		//transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
 	
 		//print ("Transform Rotate (b): " + transform.rotation + "\t" + rotation);
 		//transform.Rotate (new Vector3(270, transform.rotation.y - rotation.y, transform.rotation.z - rotation.z));
 		//transform.RotateAround (transform.position, rotation, Time.deltaTime * rotationSpeed);
 		//transform.rotation.Set (transform.rotation.w, rotation.x, transform.rotation.y, rotation.z);
-		print ("Transform Rotate (a): " + transform.rotation + "\t" + rotation);
+		//print ("Transform Rotate (a): " + transform.rotation + "\t" + rotation);
 		//transform.rotation = rotation;
 
 		// Move our position a step closer to the target.
@@ -109,5 +103,6 @@ public class EnemyAI : MonoBehaviour {
  	void Move(Transform current, Transform destination, float speed) {
 		distanceToDestination = Vector3.Distance (transform.position, wayPoint.position);
 		transform.position = Vector3.MoveTowards(current.position, destination.position, speed * Time.deltaTime);
+		//print ("Transform: " + transform.position + "\t" + distanceToDestination);
 	}
 }
