463aada08c01615cdd5cbba0d9b6e4c27bc4abad
nchou001 <nchou001@ucr.edu>
2015-01-29 20:37:10 -0800

    Added cannon on big ship
    I added a cannon onto the right side of the ship that is able to fire
    and aim left and right.

---
+++ PirateWars/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+	public float mouseSensitivity = 100f;
+	Transform myCamera;
+	float maxVertical;
+	// Use this for initialization
+	void Start () {
+		myCamera = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up * Input.GetAxis ("Mouse X") * Time.deltaTime * mouseSensitivity);
+		maxVertical += Input.GetAxis ("Mouse Y") * Time.deltaTime * mouseSensitivity;
+		maxVertical = Mathf.Clamp (maxVertical, -60, 60);
+		myCamera.localEulerAngles = Vector3.left * maxVertical;
+	}
+}
