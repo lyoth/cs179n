041f063145221ec0cafb7d60f39fa0d4502338a7
vpang002 <vpang002@ucr.edu>
2015-02-01 19:43:13 -0800

    Added cannonball collision script and minimap
    Minimap works with “Wide” layout.
    Moved health bar to work properly with “Wide” layout.
    Need to implement enemy damage for CannonballDamage.cs script.

---
+++ PirateWars/Assets/Scripts/CannonballCollision.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonballCollision : MonoBehaviour {
+	/*
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}*/
+
+	GameObject player;
+	PlayerHealth playerHealth;
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Player") 
+		{
+			//damage to player
+			player = GameObject.FindGameObjectWithTag ("Player");
+			playerHealth = player.GetComponent <PlayerHealth> ();
+			playerHealth.TakeDamage(25);
+
+			//call explosion animation?
+			//Despawn cannonball
+			Destroy (gameObject);
+		}
+
+		else if(other.tag == "Enemy")
+		{
+			//damage to enemy
+
+			//call explosion animation?
+			//despawn cannonball
+			Destroy (gameObject);
+		}
+	}
+}
