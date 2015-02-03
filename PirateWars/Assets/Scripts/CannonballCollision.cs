9e889041053928cd4e83674be152e0ea0867210e
vpang002 <vpang002@ucr.edu>
2015-02-03 00:57:11 -0800

    changed player ship and added explosions in Demo 1
    Two different explosions: one for collision with ships, another for
    collision with land.
    Dowloaded explosion VFX’s from Unity asset store

--- PirateWars/Assets/Scripts/CannonballCollision.cs
+++ PirateWars/Assets/Scripts/CannonballCollision.cs
@@ -16,6 +16,7 @@ public class CannonballCollision : MonoBehaviour {
 	GameObject player;
 	PlayerHealth playerHealth;
 	public GameObject explosion;
+	public GameObject explosion2;
 
 	void OnTriggerEnter(Collider other) 
 	{
@@ -26,20 +27,36 @@ public class CannonballCollision : MonoBehaviour {
 			playerHealth = player.GetComponent <PlayerHealth> ();
 			playerHealth.TakeDamage(25);
 
-			//call explosion animation?
+			//call explosion animation
+			Instantiate(explosion, other.transform.position, other.transform.rotation);
 			//Despawn cannonball
 			Destroy (gameObject);
-			Instantiate(explosion, other.transform.position, other.transform.rotation);
 		}
 
 		else if(other.tag == "EnemyShip")
 		{
 			//damage to enemy
 
-			//call explosion animation?
+			//call explosion animation
+			Instantiate(explosion, gameObject.transform.position, other.transform.rotation);
 			//despawn cannonball
 			Destroy (gameObject);
-			Instantiate(explosion, other.transform.position, other.transform.rotation);
+
+		}
+
+		else if (other.tag == "Terrain")
+		{
+			//call explosion animation for terrain
+			Instantiate(explosion2, gameObject.transform.position, other.transform.rotation);
+			//despawn cannonball
+			Destroy (gameObject);
+		}
+		else if (other.tag == "Water")
+		{
+			//splash!
+
+			//despawn cannonball
+			Destroy (gameObject);
 		}
 	}
 }
